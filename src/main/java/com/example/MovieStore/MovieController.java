3673a7dcbfe215f5c7a0f2284ed82583d0d8317e
HM\MAGAO <maja.gabrielsson@hm.com>
2020-11-05 16:05:27 +0100

    Added login funktionality in the controller changed the method MemberLoginMatch + the testcases for that method

--- src/main/java/com/example/MovieStore/MovieController.java
+++ src/main/java/com/example/MovieStore/MovieController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -43,12 +45,38 @@ public class MovieController {
         return result;
     }
 
-
     @GetMapping("/members")
     public String member(Model model) {
 
         List<Member> memberList = repositoryMember.memberList();
         model.addAttribute("members", memberList);
         return "members";
+    }
+    @GetMapping("/memberlogin")
+    public String loginPage(Model model) {
+        model.addAttribute("message", "Please log in");
+        return "LogInPage";
+    }
+
+    @PostMapping("/tryLogin")
+    String form(@RequestParam Integer memberID, String password, Model model, HttpSession session) {
+        Member member = MemberRepository.MemberLoginMatch(memberID, password);
+        if(member!=null){
+            session.setAttribute("member", member);
+            return "SuccessLoginPage";
+        }
+        else{
+            return "FailedLoginPage";
+        }
+
+    }
+
+
+    @GetMapping("/logout")
+    String logout(HttpSession session) {
+
+        session.removeAttribute("memberID");
+
+        return "LogInPage";
     }
 }
