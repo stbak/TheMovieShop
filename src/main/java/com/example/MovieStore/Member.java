1a96881a481a2c494d0048c27cdbe518ce507c9c
HM\MAGAO <maja.gabrielsson@hm.com>
2020-11-05 08:55:15 +0100

    changes in member, memberrepository, members.html

--- src/main/java/com/example/MovieStore/Member.java
+++ src/main/java/com/example/MovieStore/Member.java
@@ -1,2 +1,57 @@
-package com.example.MovieStore;public class Member {
+package com.example.MovieStore;
+
+public class Member {
+    int memberID;
+    String name;
+    String email;
+    String password;
+
+    public Member(int memberID, String password){
+        this.memberID=memberID;
+        this.password=password;
+    }
+
+    public Member(int memberID, String name, String email, String password){
+    this.memberID=memberID;
+    this.name=name;
+    this.email=email;
+    this.password=password;
+    }
+
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
 }
--- src/main/java/com/example/MovieStore/MemberRepository.java
+++ src/main/java/com/example/MovieStore/MemberRepository.java
@@ -1,2 +1,41 @@
-package com.example.MovieStore;public class MemberRepository {
+package com.example.MovieStore;
+
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MemberRepository {
+
+    private List<Member> members;
+
+    public MemberRepository(){
+        members = new ArrayList<>();
+
+        members.add(new Member(234123, "Kalle Anka", "Kalle@anka.com", "PWKajsaAnka!" ));
+        members.add(new Member(235222, "Pippi Långstrump", "Pippi@gmail.com", "lillaGubben123" ));
+        members.add(new Member(236323, "Tjorven", "tjorven@anka.com", "Båtsman" ));
+        members.add(new Member(237423, "Astrid Lindgren", "astrid@anka.com", "!lindgren!" ));
+
+        for (int i = 1; i <= 5; i++) {
+            members.add(new Member(200543+i, "superMoviefan" + i, "email" + i + "@gmail.com", "Password" + i));
+        }
+    }
+
+    public Member getMember(int memberid) {
+        for (Member member : members) {
+            if (member.getMemberID() == memberid) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public List<Member> memberList (){return members;}
+
+    public void addNewMember(int memberID, String password) {
+            Member member1 = new Member(memberID, password);
+            members.add(member1);
+    }
+
 }
